c874bf9cc25784861a26bac4c0ec6715973198f0
Aidan Crawley <ac15yo@brocku.ca>
2015-11-12 18:41:50 -0500

    INTRO to java

---
+++ Lab_work/javascriptexample.js
@@ -0,0 +1,17 @@
+function changeText(){
+
+document.getElementById("para").innerHTML="awesome"
+
+}
+
+
+document.write("<p> The Monkeys are everywhere </p>");
+
+
+var monkeys;
+
+monkeys = 5;
+
+monkeys= monkeys + 5;
+
+document.write(monkeys);
