ce6dff49d7ba5077f3445fd7b18659b417a469bb
Aidan Crawley <ac15yo@brocku.ca>
2015-11-26 18:39:22 -0500

    JAVA

---
+++ Lab_work/NEWJAVA.js
@@ -0,0 +1,57 @@
+
+var amazon = new Array();
+
+amazon[0]="monkeys";
+amazon[1]="computers";
+amazon[2]="arduino";
+amazon[3]="256";
+amazon[4]="hello";
+
+amazon.push("hats");
+
+amazon.unshift();
+
+var length = amazon.length;
+
+console.log(length);
+
+document.write("amazon");
+
+console.log(amazon);
+
+
+for(count=0; count <5; count+=1){
+
+  console.log(amazon[count]);
+}
+
+var left = 10;
+var right = 5;
+
+
+if(left == right){
+  console.log("hurrah1");
+
+}
+
+
+if(left == right){
+  console.log("hurrah2");
+}
+
+
+
+function aFace(){
+
+
+var aVariable = 10;
+aVariable=+10;
+aVariable-=5;
+
+return (aVariable);
+
+}
+
+var myVariable = aFace();
+
+console.log(myVariable);
--- Lab_work/javascriptexample.js
+++ Lab_work/javascriptexample.js
@@ -1,17 +1,30 @@
-function changeText(){
+alert("this is some content")
 
-document.getElementById("para").innerHTML="awesome"
+confirm("Do you want a taco?")
+
+
+console.log("This is some awesome text");
+
+
+prompt("whats your favorite band?")
+
+function theDate(){
+
+
+  var todayDate = new Date();
+  var month = todayDate.getMonth();
+  var year = todayDate.getFullYear();
+  var day = todayDate.getDate();
+
+
+document.getElementById("para").innerHTML=+month+day+year;
+
 
 }
 
 
-document.write("<p> The Monkeys are everywhere </p>");
 
+function theName{
+   var aName = document.getElementById("sometext")
 
-var monkeys;
-
-monkeys = 5;
-
-monkeys= monkeys + 5;
-
-document.write(monkeys);
+}
